caed28f86ce04cba256bb8a07d717dc41e500ccb
Raquel Souza <rakellkizz@gmail.com>
2025-05-09 20:57:15 -0300

    ObemTeQuerApiApplication.java

--- logins/com/rakellkizz/obemquetequer/api/config/ObemqueTeQuerApiApplication.java
+++ logins/com/rakellkizz/obemquetequer/api/config/ObemqueTeQuerApiApplication.java
@@ -1,4 +1,4 @@
-package com.rakellkizz.obemquetequer.api.config;
+package com.rakellkizz.obemtequer.api.config;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -14,7 +14,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * RESTful.
  */
 @SpringBootApplication(scanBasePackages = "com.rakellkizz.obemtequer.api")
-public class ObemqueTeQuerApiApplication {
+public class ObemTeQuerApiApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ObemqueTeQuerApiApplication.class, args);
