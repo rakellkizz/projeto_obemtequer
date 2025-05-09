81e259b1d05e4711ac096e3180c5f1f3ca1f3f42
Raquel Souza <rakellkizz@gmail.com>
2025-05-09 20:59:44 -0300

    Mensagem.java

--- logins/com/rakellkizz/obemquetequer/api/model/Mensagem.java
+++ logins/com/rakellkizz/obemquetequer/api/model/Mensagem.java
@@ -1,4 +1,4 @@
-package com.rakellkizz.obemquetequer.api.model;
+package com.rakellkizz.obemtequer.api.model;
 
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
