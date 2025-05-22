6c046c47e00394050eae777f8ad8318e3ec4c78a
RakellKizz <rakellkizz@gmail.com>
2025-05-22 13:44:12 -0300

    fix: unificando frontend e backend no repositório principal

--- backend/apps/app_run.py
+++
@@ -1,4 +0,0 @@
-import streamlit as st
-
-st.title("Projeto O Bem Te Quer 💛")
-st.write("Olá, Raquel! Seu app está funcionando com Streamlit!")
--- backend/jest.backend.config.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  testEnvironment: 'node',
-  testMatch: ['**/backend/**/*.test.js'],
-  // outras configurações específicas para backend podem ser adicionadas aqui
-};
